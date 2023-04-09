1b7a0e02778bf7c26b62d46a483c86afb392d8f2
aldo <aldo@jjindonesia.com>
2023-04-09 11:58:02 +0700

    profile screen done

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -48,9 +48,8 @@ const Auth = () => {
         email,
         password,
         redirect: false,
-        callbackUrl: "/",
+        callbackUrl: "/profile",
       });
-      router.push("/");
     } catch (error) {
       console.log(error);
     }
@@ -110,13 +109,13 @@ const Auth = () => {
               <div className="flex items-center gap-4 mt-8 justify-center">
                 <div
                   className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
-                  onClick={() => signIn("google", { callbackUrl: "/" })}
+                  onClick={() => signIn("google", { callbackUrl: "/profile" })}
                 >
                   <FcGoogle size={30} />
                 </div>
                 <div
                   className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
-                  onClick={() => signIn("github", { callbackUrl: "/" })}
+                  onClick={() => signIn("github", { callbackUrl: "/profile" })}
                 >
                   <FaGithub size={30} />
                 </div>
---
+++ pages/profile.tsx
@@ -0,0 +1,50 @@
+import { getServerSession } from "next-auth";
+import React from "react";
+import { authOptions } from "./api/auth/[...nextauth]";
+import { NextPageContext } from "next";
+import useCurrentuser from "@/hooks/useCurrentUser";
+import Link from "next/link";
+export async function getServerSideProps(context: NextPageContext) {
+  const session = getServerSession(context.req, context.res, authOptions);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+  }
+  return { props: {} };
+}
+
+function Profile() {
+  const { data: user, error, isLoading, mutate } = useCurrentuser();
+  return (
+    <div className="flex items-center h-full justify-center">
+      <div className="flex flex-col ">
+        <h1 className="text-3xl md:text-6xl text-white text-center">
+          Who is watching?
+        </h1>
+        <div className="flex items-center justify-center gap-8 mt-10">
+          <Link href={"/"}>
+            <div className="group flex-row w-44 mx-auto">
+              <div className="w-44 h-44 rounded-md flex items-center justify-center border-2 border-transparent group-hover:cursor-pointer group-hover:border-white">
+                <img
+                  src="/images/default-red.png"
+                  alt="profile"
+                  className="rounded-md"
+                />
+              </div>
+              <div className="mt-4 text-gray-400 text-2xl text-center group-hover:text-white group-hover:cursor-pointer">
+                <p>{user?.name}</p>
+              </div>
+            </div>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Profile;
