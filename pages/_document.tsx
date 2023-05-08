285592d04d135eefa08f2d99a529dc2a634c6002
aldo <aldo@jjindonesia.com>
2023-05-08 10:47:36 +0700

    add chatbot

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,19 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script
+          async
+          defer
+          src="https://webuild.wetalk.id/js/widget/knkfmify9fafm7vl/float.js"
+        ></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
